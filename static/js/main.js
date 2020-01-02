4a8d06f2a5ba0756ff02bc1aab9534994ebe6298
sage <laymonage@gmail.com>
2020-01-02 20:47:43 +0700

    Add dark mode

--- static/js/main.js
+++ static/js/main.js
@@ -1,36 +1,58 @@
 // To make images retina, add a class "2x" to the img element
 // and add a <image-name>@2x.png image. Assumes jquery is loaded.
- 
+
 function isRetina() {
 	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
 					  (min--moz-device-pixel-ratio: 1.5),\
 					  (-o-min-device-pixel-ratio: 3/2),\
 					  (min-resolution: 1.5dppx)";
- 
+
 	if (window.devicePixelRatio > 1)
 		return true;
- 
+
 	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
 		return true;
- 
+
 	return false;
 };
- 
- 
+
+
 function retina() {
-	
+
 	if (!isRetina())
 		return;
-	
+
 	$("img.2x").map(function(i, image) {
-		
+
 		var path = $(image).attr("src");
-		
+
 		path = path.replace(".png", "@2x.png");
 		path = path.replace(".jpg", "@2x.jpg");
-		
+
 		$(image).attr("src", path);
 	});
 };
- 
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.getElementById('dark-css').disabled = false;
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.getElementById('dark-css').disabled = true;
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme && currentTheme === 'dark') {
+	toggleSwitch.checked = true;
+	document.getElementById('dark-css').disabled = false;
+}
+
 $(document).ready(retina);
